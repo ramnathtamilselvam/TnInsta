36da4204405718ac1a5bbff5100422e53646c019
ramnatht <ramnath.t@optisolbusiness.com>
2021-02-22 13:15:13 +0530

    First Commit

---
+++ TNInsta/TNInsta/TNInstaApp.swift
@@ -0,0 +1,20 @@
+//
+//  TNInstaApp.swift
+//  TNInsta
+//
+//  Created by MAC-OBS-27 on 22/02/21.
+//
+
+import SwiftUI
+
+@main
+struct TNInstaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
